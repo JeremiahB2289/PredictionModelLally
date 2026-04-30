39b8fb0f93db81ae84bc77877c75df103e7a9ede
JeremiahB2289 <biggsj3@rpi.edu>
2026-04-30 16:09:52 -0400

    Oops
    Removed u because uimport should be import

--- backend/app.py
+++ backend/app.py
@@ -119,7 +119,7 @@ def refresh():
 if __name__ == "__main__":
     import uvicorn
 
-    uimport os
+    import os
 
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
